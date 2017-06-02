11fe0028bcaefa88914020f4ceae8ac787e82c04
Darryl Yeo <darryl@darryl-yeo.com>
2017-06-01 20:29:34 -0700

    Add anchor links to headings; ensure they are not styled like links in body text

--- src/framework/post-content-sections.php
+++ src/framework/post-content-sections.php
@@ -1,9 +1,11 @@
 <?php
 add_filter('the_content', function($content){
 	$content = '<section id="top">'.preg_replace_callback(
-		'/<h2(.*?)>(.*?)<\/h2>/',
+		'/<h2(?:.*?)>(.*?)<\/h2>/',
 		function($matches){
-			return '</section><section id="'.sanitize_title($matches[2]).'"><h2'.$matches[1].'>'.$matches[2].'</h2>';
+			$title = $matches[1];
+			$id = sanitize_title($title);
+			return "</section><section id={$id}><h2><a href=#{$id}>{$title}</a></h2>";
 		},
 		$content
 	).'</section>';
